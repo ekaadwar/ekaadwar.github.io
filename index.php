bd988310e4aecf2d87ce95f764344dbb42456ec4
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-03-06 17:42:31 +0700

    change color of jumbotron section to darker

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   </head>
   <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient shadow-sm">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark bg-gradient shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="#">Eka Fajhari Adwar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -42,10 +42,10 @@
     <!-- Navbar Ended -->
 
     <!-- jumbotron started -->
-    <section class="jumbotron text-center">
+    <section id="home" class="jumbotron text-center">
       <img src="img/ekafajhariadwar.jpg" alt="Eka Fajhari Adwar" width="200" class="rounded-circle img-thumbnail">
       <h1 class="display-4">Eka Fajhari Adwar</h1>
-      <p class="lead">Physicist | Fullstack Programmer</p>
+      <p class="lead fw-bold">Physicist | Fullstack Programmer</p>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#ffffff" fill-opacity="1" d="M0,64L40,85.3C80,107,160,149,240,170.7C320,192,400,192,480,170.7C560,149,640,107,720,122.7C800,139,880,213,960,229.3C1040,245,1120,203,1200,208C1280,213,1360,267,1400,293.3L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
       </svg>
