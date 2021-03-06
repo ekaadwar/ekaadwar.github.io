d90be2e247f8b46e35c345203e84a37794099211
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-03-06 19:19:18 +0700

    add breakpoint for mobile display in About Section

--- index.php
+++ index.php
@@ -11,23 +11,26 @@
     <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
 
+    <!-- icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
     <title>My Portfolio|Eka Fajhari Adwar</title>
   </head>
   <body>
     <!-- Navbar -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark bg-gradient shadow-sm">
       <div class="container">
-        <a class="navbar-brand" href="#">Eka Fajhari Adwar</a>
+        <a class="navbar-brand" href="#home">Eka Fajhari Adwar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="#home">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="#about">About</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Project</a>
@@ -51,6 +54,31 @@
       </svg>
     </section>
     <!-- jumbotron ended -->
+
+    <!-- section about started -->
+    <section id="about">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col text-center">
+            <h1>About Me</h1>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-sm-4">
+            <p>Hai, perkenalkan saya <span class="fw-bolder">Eka Fajhari Adwar</span>. Anda dapat menyapa saya dengan Eka. Saya adalah sarjana lulusan dari Fisika FMIPA Universitas Andalas. Saya menyukai dunia instrumentasi dan pemrograman. Salah satu pemrograman yang paling saya sukai adalah <span class="fw-bolder">Pemrograman Website</span>.</p>
+            <p>Saya berasal dari kabupaten Pasaman, Sumatera Barat - Indonesia. Saya dilahirkan di Lubuk Sikaping pada tanggal 27 September 1996.</p>
+          </div>
+          <div class="col-sm-4">
+          <p class="fst-italic"> Hi, introduce me <span class = "fw-bolder"> Eka Fajhari Adwar </span>. You can greet me with Eka. I am a graduate of Physics, Faculty of Mathematics and Natural Sciences, Andalas University. I love the world of instrumentation and programming. One of my favorite programming is <span class = "fw-bolder"> Website Programming </span>.</p>
+          <p> I come from Pasaman district, West Sumatra - Indonesia. I was born in Lubuk Sikaping on September 27, 1996.</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#ccc" fill-opacity="1" d="M0,128L30,138.7C60,149,120,171,180,154.7C240,139,300,85,360,96C420,107,480,181,540,224C600,267,660,277,720,245.3C780,213,840,139,900,128C960,117,1020,171,1080,208C1140,245,1200,267,1260,272C1320,277,1380,267,1410,261.3L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
+      </svg>
+    </section>
+    <!-- section about ended -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
